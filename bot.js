addc508047f526b27d13d80e27633c0d1ee26024
faresmnasri <31959540+faresmnasri@users.noreply.github.com>
2019-01-13 02:20:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Eris = require("eris");
 var bot = new Eris(process.env.BOT_TOKEN);
-var id = "531929984869203968";
+var id = "533816327278952459";
 //** لاتعدل على شي تحت ذا **//
 bot.on("ready", () => {
 onstart();
