fc5f5d2cc1a04abfc92d15d1b030d2406501a2fa
faresmnasri <31959540+faresmnasri@users.noreply.github.com>
2019-01-13 02:19:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,35 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const Eris = require("eris");
+var bot = new Eris(process.env.BOT_TOKEN);
+var id = "531929984869203968";
+//** لاتعدل على شي تحت ذا **//
+bot.on("ready", () => {
+onstart();
 });
 
+function onstart(){
 
-client.login(process.env.BOT_TOKEN);
+setInterval(function(){
+    
+var currentTime = new Date(),
+hours = currentTime.getHours() + 1 ,
+minutes = currentTime.getMinutes(),
+seconds = currentTime.getSeconds() + 35 ;
+    
+if (minutes < 10) {
+minutes = "0" + minutes;
+}
+
+if (hours >= 24) {
+
+hours = hours - 24;
+}
+if (hours == 24) {
+hours = 24;
+}
+    
+bot.editChannel(id, { name :"   ●⌠" + hours +  ":"  + minutes + "⌡●   "});
+}, 60000);
+    
+}
+    
+bot.connect();
